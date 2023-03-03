6bc02779137d7c8d2859243a2b2119687eb4c376
Joe Boylson <joeboylson@gmail.com>
2023-03-03 00:08:57 -0500

    adding deployment details

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -1,1 +1,5 @@
-export const SERVER_URL = process.env.REACT_APP_SERVER_URL;
+import { isEqual } from "lodash";
+
+export const SERVER_URL = isEqual(process.env.REACT_APP_SERVER_URL, "NULL")
+  ? ""
+  : process.env.REACT_APP_SERVER_URL;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { SERVER_URL } from "./constants";
 
 // import global styles
 import "./index.css";
+
+console.group("::: init");
+console.log({ SERVER_URL });
+console.groupEnd();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
